8337d65cb71bad03368e8d48c18bcdbbb4d110dc
marc-win <marc.gehring@web.de>
2020-10-17 13:10:50 +0200

    filtering dataset
    implemented
    * dropping of text non numerical columns
    * combing data from all countries
    * pickling dataset

--- youtube.py
+++ youtube.py
@@ -12,7 +12,29 @@ nargin = len(sys.argv)-1
 argin = sys.argv[1:]
 
 # parameter
-WORKDIR = r"C:\Users\Marc\Documents\python_projects\machine_learning"
+SOURCE = r"C:\Users\Marc\Documents\python_projects\machine_learning"
+
+# merge country data from source to numerical dataset
+if nargin > 0 and argin[0] == "merge":
+	all_countries_numerical = pd.DataFrame()
+	files = glob.glob(SOURCE+r"\dataset\*.csv")
+	for file in files:
+		filepath, filename = os.path.split(file)
+		country_data = pd.read_csv(file)
+		country_data.drop(["title","channel_title","tags","thumbnail_link","description"],axis="columns",inplace = True)
+		country_data["country"] = filename[0:2]
+		all_countries_numerical = all_countries_numerical.append(country_data)
+
+	time_to_trends = []
+	for publish_string, trending_string in zip(all_countries_numerical["publish_time"].values,all_countries_numerical["trending_date"].values):
+		publish_date = datetime.strptime(publish_string,"%Y-%m-%dT%H:%M:%S.%fZ").date()
+		trending_date = datetime.strptime(trending_string,"%y.%d.%m").date()
+		delta = trending_date - publish_date
+		time_to_trends.append(delta.days)
+	all_countries_numerical["days_to_trends"] = time_to_trends
+	all_countries_numerical.reset_index()
+	all_countries_numerical.to_pickle("./dataset.pkl")
+
 
 # write time series from dataset
 if nargin > 0 and argin[0] == "timeseries":
@@ -24,6 +46,7 @@ if nargin > 0 and argin[0] == "timeseries":
 		print(len(dataframe.index))
 		print(dataframe.columns)
 		time_series = time_series_from_dataset(dataframe,"likes")
+
 
 
 
