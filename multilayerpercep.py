e75d2f3d6eab74af6f205122558952428f45943d
marc-win <marc.gehring@web.de>
2021-01-17 16:02:56 +0100

    norm, center, standardize samplewise seems to be counterproductive

--- multilayerpercep.py
+++ multilayerpercep.py
@@ -11,11 +11,23 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
 """
-# get input arguments
-no input: eval last trained model
-train: train new model
+arguments and parameters
+no positional arg: eval last trained model
+"train": train new model
+
+SOURCE: directory with subdirs for every category
+SAVE_MODEL: path to directory for saved models
+CATEGORIES: list of category numbers to train and test on, list of int
+MODEL_SUFFIX: suffix added to output files (lossfunction and test eval)
 """
+
+SOURCE = r"C:/Users/Marc/Documents/python_projects/machine_learning/thumbnails/"
+SAVE_MODEL = r"C:\Users\Marc\Documents\python_projects\machine_learning\saved_models"
+CATEGORIES = [17,2] # Pets vs. Cars [15,2] # Sport vs Cars [17,2]
+MODEL_SUFFIX = "norm_center_standard"
 
 argsin = sys.argv[1:]
 nargsin = len(argsin)
@@ -24,12 +36,7 @@ nargsin = len(argsin)
 prepare the data
 """
 
-SOURCE = r"C:/Users/Marc/Documents/python_projects/machine_learning/thumbnails/"
-SAVE_MODEL = r"C:\Users\Marc\Documents\python_projects\machine_learning\saved_models"
-CATEGORIES = [17,2] # Pets vs. Cars [15,2] # Sport vs Cars [17,2]
-number_of_categories=len(CATEGORIES)
-
-# pick samples from category folders
+# pick samples from category subdirectories
 number_of_samples = []
 videos_in_dir = []
 for directory in CATEGORIES:
@@ -45,19 +52,27 @@ for directory in CATEGORIES:
 	number_of_samples.append(len(files_in_dir))
 	videos_in_dir.append(files_in_dir)
 
+# print out stats
 min_samples = min(number_of_samples)
 print("number of samples:",min_samples)
 
-# preparing images
+# loading image data and arranging to arrays
 thumbnail_samples = []
 y_category = []
+number_of_categories=len(CATEGORIES)
 for category,idx in zip(CATEGORIES,range(number_of_categories)):
 	print("++++++",category,idx,"+++++")
 	samples = videos_in_dir[idx]
 
 	for thumbnail_file in samples[:min_samples]:
 		thumbnail = cv2.imread(SOURCE+str(category)+r'/'+thumbnail_file) #Reading the thumbnail (OpenCV)
-		thumbnail_samples.append(np.asarray(thumbnail))
+
+		# normalize, center samplewise and globally, standardize globally
+		thumbnail_sample_norm = np.asarray(thumbnail)/255.0
+		thumbnail_sample = (thumbnail_sample_norm - thumbnail_sample_norm.mean())/thumbnail_sample_norm.std()
+
+		# append to dataset
+		thumbnail_samples.append(thumbnail_sample)
 		truth_for_image = np.where(np.array(CATEGORIES)==category,1,0)
 		y_category.append(truth_for_image)
 
@@ -66,6 +81,9 @@ x = np.asarray(thumbnail_samples)
 y = np.asarray(y_category)
 print("shape of input array:",x.shape)
 print("shape of output array:",y.shape)
+
+#print(x[0].min(),x[0].max(),x[0].mean(),x[0].std())
+
 
 # split the data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
@@ -94,18 +112,17 @@ if nargsin > 0 and argsin[0] == "train":
 	batch_size = 64
 
 	# number of times complete samples are passed trough the network
-	epochs = 40
+	epochs = 50
 
-	#model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 	model.compile(optimizer=Optimizer.SGD(lr=1e-4),loss='categorical_crossentropy',metrics=['accuracy'])
 	model.summary()
 
-	csv_logger = CSVLogger('./data/thumbnail_stats/thumbnail_training.log', separator=',', append=False)
+	csv_logger = CSVLogger('./data/thumbnail_stats/thumbnail_training_'+MODEL_SUFFIX+'.log', separator=',', append=False)
 	history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,callbacks=[csv_logger],validation_split=0.1)	# ,callbacks=[plot_losses]  plot_losses = PlotLosses()
-	model.save(SAVE_MODEL)
+	model.save(SAVE_MODEL+"/"+MODEL_SUFFIX)
 
 else:
-	model = keras.models.load_model(SAVE_MODEL)
+	model = keras.models.load_model(SAVE_MODEL+"/"+MODEL_SUFFIX)
 
 """
 ## Evaluate the trained model
@@ -114,19 +131,19 @@ else:
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
-test_results = open("./data/thumbnail_stats/thumbnail_test.txt","w+")
+test_results = open("./data/thumbnail_stats/thumbnail_test_"+MODEL_SUFFIX+".txt","w+")
 test_results.write("{}\t{}\n".format( score[0], score[1]))
 test_results.close()
 
 # print latest loss fcn
-csv_history = pd.read_csv('./data/thumbnail_stats/thumbnail_training.log')
+csv_history = pd.read_csv('./data/thumbnail_stats/thumbnail_training_'+MODEL_SUFFIX+'.log')
 csv_history.set_index("epoch")
 print(csv_history.columns)
 csv_history.drop('epoch',axis=1,inplace=True)
-csv_history.plot(logy=True)
+csv_history.plot()#logy=True
 plt.title('model performance')
 plt.ylabel('quantity')
 plt.xlabel('epoch')
 plt.legend(loc='upper right')
-plt.savefig("./data/figures/mlp/lossfunction.png")
+plt.savefig("./data/figures/mlp/lossfunction_"+MODEL_SUFFIX+".png")
 plt.show()
