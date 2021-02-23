f9e363830e1f2199e9f45bbc80d486e10f03469d
marc-win <marc.gehring@web.de>
2021-02-23 15:39:38 +0100

    running but shitty results

--- vae_autoencoder.py
+++ vae_autoencoder.py
@@ -8,20 +8,119 @@ from keras.layers import Input, Dense, Lambda, Layer, Add, Multiply
 from keras.models import Model, Sequential
 from keras.datasets import mnist
 
+import cv2
+from sklearn.model_selection import train_test_split
+from skimage import color
+from PIL import Image
+
 import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-import faulthandler
-faulthandler.enable()
-
-original_dim = 784
-intermediate_dim = 256
+original_dim = 10800
+intermediate_dim = 512
 latent_dim = 2
 batch_size = 100
-epochs = 10
+epochs = 50
 epsilon_std = 1.0
+
+# data loader
+dataset = "yt"
+
+if dataset == "yt":
+    # parameters
+    SOURCE = r"C:/Users/Marc/Documents/python_projects/machine_learning/thumbnails/"
+    CATEGORIES = [25]
+
+    # load dataset
+    # pick samples from category subdirectories
+    number_of_samples = []
+    videos_in_dir = []
+    for directory in CATEGORIES:
+        path = SOURCE+str(directory)
+        files_in_dir = os.listdir(path)
+
+        # remove windows settings file from dataset
+        try:
+            files_in_dir.remove("Thumbs.db")
+        except:
+            pass
+
+        number_of_samples.append(len(files_in_dir))
+        videos_in_dir.append(files_in_dir)
+
+    # print out stats
+    min_samples = min(number_of_samples)
+    print("number of samples:",min_samples)
+
+    # loading image data and arranging to arrays
+    thumbnail_samples = []
+    y_category = []
+    number_of_categories=len(CATEGORIES)
+    for category,idx in zip(CATEGORIES,range(number_of_categories)):
+        thumbnail_samples_per_category = []
+        print("++++++",category,idx,"+++++")
+        samples = videos_in_dir[idx]
+
+        for thumbnail_file in samples[:min_samples]:
+            thumbnail = cv2.imread(SOURCE+str(category)+r'/'+thumbnail_file) #Reading the thumbnail (OpenCV)
+
+            thumbnail = color.rgb2gray(thumbnail)
+            thumbnail_sample = np.reshape(np.asarray(thumbnail.astype("float32")/255.),original_dim)
+            #thumbnail_sample.resize((96,120,3))
+
+            # append to dataset
+            thumbnail_samples.append(thumbnail_sample)      #_per_category
+
+            #truth_for_image = np.where(np.array(CATEGORIES)==category,1,0)
+            y_category.append(category)
+
+    # make input arrays
+    x = np.asarray(thumbnail_samples)
+    y = np.asarray(y_category)
+    print("shape of input array:",x.shape)
+    print("shape of output array:",y.shape)
+
+elif dataset == "dogs":
+
+    SOURCE = r"C:/Users/Marc/Documents/python_projects/machine_learning/all-dogs/"
+    image_samples = []
+
+    # CREATE RANDOMLY CROPPED IMAGES
+    for i in range(500000):
+        img = Image.open(SOURCE + IMAGES[i%len(IMAGES)])
+        img = img.resize(( 100,int(img.size[1]/(img.size[0]/100) )), Image.ANTIALIAS)
+        w = img.size[0]; h = img.size[1]; a=0; b=0
+        if w>64: a = np.random.randint(0,w-64)
+        if h>64: b = np.random.randint(0,h-64)
+        img = img.crop((a, b, 64+a, 64+b))
+        img.save('../cropped/'+str(i)+'.png','PNG')
+        if i%100000==0: print('created',i,'cropped images')
+        print('created 500000 cropped images')
+
+    for file in os.listdir(SOURCE):
+
+        img = img.resize(( 100,int(img.size[1]/(img.size[0]/100) )), Image.ANTIALIAS)
+        w = img.size[0]; h = img.size[1]; a=0; b=0
+        if w>64: a = np.random.randint(0,w-64)
+        if h>64: b = np.random.randint(0,h-64)
+        img = img.crop((a, b, 64+a, 64+b))
+        img.save('../cropped/'+str(i)+'.png','PNG')
+
+        image = cv2.imread(SOURCE+file) #Reading the image (OpenCV)
+        image_sample = np.reshape(np.asarray(image.astype("float32")/255.),original_dim)
+
+        # append to dataset
+        image_samples.append(image_sample)      #_per_category
+
+    x = np.asarray(thumbnail_samples)
+    print("shape of input array:",x.shape)
+
+
+# split the data
+x_train, x_test,y_train, y_test = train_test_split(x,y, test_size=0.3, random_state=42)
+print(x_train.shape, x_test.shape)
 
 
 def nll(y_true, y_pred):
@@ -79,11 +178,6 @@ print(x_pred.shape)
 vae = Model(inputs=[x, eps], outputs=x_pred)
 vae.compile(optimizer='rmsprop', loss=nll)
 
-# train the VAE on MNIST digits
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(-1, original_dim) / 255.
-x_test = x_test.reshape(-1, original_dim) / 255.
-
 vae.fit(x_train,x_train,shuffle=True,epochs=epochs,batch_size=batch_size,validation_data=(x_test, x_test))
 
 encoder = Model(x, z_mu)
@@ -94,11 +188,9 @@ plt.figure(figsize=(6, 6))
 plt.scatter(z_test[:, 0], z_test[:, 1], c=y_test,
             alpha=.4, s=3**2, cmap='viridis')
 plt.colorbar()
-plt.show()
 
 # display a 2D manifold of the digits
 n = 15  # figure with 15x15 digits
-digit_size = 28
 
 # linearly spaced coordinates on the unit square were transformed
 # through the inverse CDF (ppf) of the Gaussian to produce values
@@ -108,7 +200,7 @@ u_grid = np.dstack(np.meshgrid(np.linspace(0.05, 0.95, n),
                                np.linspace(0.05, 0.95, n)))
 z_grid = norm.ppf(u_grid)
 x_decoded = decoder.predict(z_grid.reshape(n*n, 2))
-x_decoded = x_decoded.reshape(n, n, digit_size, digit_size)
+x_decoded = x_decoded.reshape(n, n, 90, 120)
 
 plt.figure(figsize=(10, 10))
 plt.imshow(np.block(list(map(list, x_decoded))), cmap='gray')
